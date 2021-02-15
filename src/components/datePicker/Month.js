9fc7094912e65af1eb5d7040858c9a967daf42d7
Jonathan Beausaert <jonathanbeausaert@gmail.com>
2021-02-15 16:22:28 +0100

    Removed division in weeks

--- src/components/datePicker/Month.js
+++ src/components/datePicker/Month.js
@@ -1,33 +1,18 @@
-import React from 'react'
-import { getMonthCalendar } from '../_utils'
-import Day from './Day'
+import React from 'react';
+import { getMonthCalendar } from '../_utils';
+import Day from './Day';
 
 function Month(props) {
-   const { activeMonth } = props
-   const calendar = getMonthCalendar(activeMonth)
+	const { activeMonth } = props;
+	const calendar = getMonthCalendar(activeMonth);
 
-   const divideInWeeks = (calendarArr) => {
-      const nCalendar = [...calendarArr]
-      const arr = []
-
-      while (nCalendar.length > 0) {
-         const week = nCalendar.splice(0, 7)
-         arr.push(week)
-      }
-      return arr
-   }
-
-   return (
-      <div className="datePicker__body">
-         {divideInWeeks(calendar).map((week, i) => (
-            <div key={i} className="datePicker__week datePicker__row">
-               {week.map((day, i) => (
-                  <Day key={i} day={day} {...props} />
-               ))}
-            </div>
-         ))}
-      </div>
-   )
+	return (
+		<div className="datePicker__body">
+			{calendar.map((day, i) => (
+				<Day key={i} day={day} {...props} />
+			))}
+		</div>
+	);
 }
 
-export default Month
+export default Month;
